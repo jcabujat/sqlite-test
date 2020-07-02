e6950bde8ecfd6e1f37eb08645d53de2cff15dff
jonathancabujat <jonathancabujat@gmail.com>
2020-07-02 11:11:08 +0800

    Initial commit: - code for executing SQL codes - created SQLite test database - inserted some records and queried them to display in a simple toast message.

---
+++ app/src/main/java/com/example/sqlitetest/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.sqlitetest;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        SQLiteDatabase sqLiteDatabase = getBaseContext().openOrCreateDatabase("sqlite-test.db", MODE_PRIVATE, null);
+        String sql = "CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT);";
+        Log.d(TAG, "onCreate: sql command = " + sql);
+        sqLiteDatabase.execSQL(sql);
+        sql = "INSERT INTO contacts VALUES ('Tim', 6456789, 'tim@email.com');";
+        Log.d(TAG, "onCreate: sql command = " + sql);
+        sqLiteDatabase.execSQL(sql);
+        sql = "INSERT INTO contacts VALUES ('Fred', 12345, 'fred@nurk.com');";
+        Log.d(TAG, "onCreate: sql command = " + sql);
+        sqLiteDatabase.execSQL(sql);
+
+        sql = "SELECT * FROM contacts;";
+        Cursor query = sqLiteDatabase.rawQuery(sql, null);
+        if (query.moveToFirst()) {
+            do {
+                String name = query.getString(0);
+                int phone = query.getInt(1);
+                String email = query.getString(2);
+                Toast.makeText(this, "Name: " + name + ", phone: " + phone +
+                        ", email: " + email, Toast.LENGTH_LONG).show();
+            } while (query.moveToNext());
+        }
+        query.close();
+        sqLiteDatabase.close();
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
